eb927c81330923fd5ce13a7a26f6ef4db0c374e1
taeminpark2020 <volta2030@gmail.com>
2020-12-20 08:30:35 +0900

    firebase-web-test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>안녕하세요</p>
+      <Button primary> 하이하이 </Button>
     </div>
   );
 }
